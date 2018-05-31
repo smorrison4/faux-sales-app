52b9c407b1cf8f39390a3334f4d4255e8f199ac5
Steve Morrison <smorrison4@aol.com>
2018-05-31 00:01:09 -0500

    unsaved changes put in

---
+++ src/app/Transaction-type.enum.ts
@@ -0,0 +1,9 @@
+export enum TransactionType {
+    WalkInOrder = 'Walk in Order',
+    SalesOrder = 'Sales Order',
+    Adjustment = 'Adjustment',
+    Return = 'Return',
+    Inventory = 'Inventory', 
+    Shipment = 'Shipment',
+    Receipt = 'Receipt'
+}
